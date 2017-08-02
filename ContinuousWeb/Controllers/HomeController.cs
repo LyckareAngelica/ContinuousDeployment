1377ddf8f10e91261ab0fb5f8b9b4c950c5a032e
LyckareAngelica <angelica.lyckare@gmail.com>
2017-08-02 11:34:21 +0200

    undo the failure

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -14,11 +14,9 @@ namespace ContinusWeb.Controllers
         }
 
         public IActionResult About()
-        {
-            throw new NotImplementedException();
-            
-            //ViewData["Message"] = "Your application description page.";
-            //return View();
+        {        
+            ViewData["Message"] = "Your application description page.";
+            return View();
         }
 
         public IActionResult Contact()
